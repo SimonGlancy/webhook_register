42dcdd463a7c2328ea39035f066a5e647db46a4f
Simon T Glancy <si.glancy@gmail.com>
2016-08-19 10:14:20 +0100

    first test written

---
+++ spec/unit/webhooks_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe WebhooksController, 'testing routes' do
+  before(:each) do
+    @webhook1 = Email.create("address":"barney@lostmy.name","email_type":"Shipment","event":"send","timestamp":1432820696)
+    @webhook2 = Email.create("address":"tom@lostmy.name","email_type":"UserConfirmation","event":"click","timestamp":1432820702)
+    @webhook3 = Email.create("address":"vitor@lostmy.name","email_type":"Shipment","event":"open","timestamp":1432820704)
+  end
+
+
+  describe '#index' do
+    it 'shows all webhooks in the database', type: :request do
+      get '/'
+      json = JSON.parse(response.body)
+      expect(json.length).to eq(3)
+    end
+  end
+end
